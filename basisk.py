e0840d5a9bcb3d52fe62fc2e6184040a5566f736
Trulle1234 <leonard.trulsson@gmail.com>
2025-10-29 16:32:51 +0100

    i cant bother doing more today :()

--- basisk.py
+++ basisk.py
@@ -429,7 +429,7 @@ class Parser:
         if not res.error and self.current_tok.type != TT_EOF:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Förväntade \"+\", \"-\", \"*\" eller \"/\" "
+                "Förväntade \"+\", \"-\", \"*\" eller \"/\""
             ))
         return res
     
@@ -443,7 +443,7 @@ class Parser:
         if not self.current_tok.matches(TT_KEYWORD, "om"):
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Förväntade \"OM\""
+                "Förväntade \"om\""
             ))
         
         res.register_advancement()
@@ -456,7 +456,7 @@ class Parser:
         if not self.current_tok.matches(TT_KEYWORD, "då"):
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Förväntade \"DÅ\""
+                "Förväntade \"då\""
             ))
          
         res.register_advancement()
@@ -476,7 +476,7 @@ class Parser:
             if not self.current_tok.matches(TT_KEYWORD, "då"):
                 return res.failure(InvalidSyntaxError(
                     self.current_tok.pos_start, self.current_tok.pos_end,
-                    "Förväntade \"DÅ\""
+                    "Förväntade \"då\""
                 ))
             
             res.register_advancement()
@@ -494,6 +494,98 @@ class Parser:
             if res.error: return res
         
         return res.success(IfNode(cases, else_case))
+    
+    def for_expr(self):
+        res = ParseResult()
+
+        if not self.current_tok.matches(TT_KEYWORD, "för"):
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Förväntade \"för\""
+            ))
+        
+        res.register_advancement()
+        self.advance()
+
+        if self.current_tok.type != TT_IDENTIFER:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Förväntade identifierare"
+            ))
+        
+        var_name = self.current_tok
+        res.register_advancement()
+        self.advance()
+
+        if self.current_tok.type != TT_EQ:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Förväntade \"=\""
+            ))
+        
+        res.register_advancement()
+        self.advance()
+
+        start_value = res.register(self.expr())
+        if res.error: return res
+
+        if not self.current_tok.matches(TT_KEYWORD, "till"):
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Förväntade \"till\""
+            ))
+        
+        res.register_advancement()
+        self.advance()
+
+        end_value = res.register(self.expr())
+        if res.error: return res
+
+        if self.current_tok.matches(TT_KEYWORD, "steg"):
+            res.register_advancement()
+            self.advance()
+
+            step_value = res.register(self.expr())
+            if res.error: return res
+        else:
+            step_value = None
+        
+        if not self.current_tok.matches(TT_KEYWORD, "då"):
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Förväntade \"då\""
+            ))
+        
+        res.register_advancement()
+        self.advance() 
+
+        body = res.register(self.expr())
+        self.advance()
+
+        return res.success(ForNode(var_name, start_value, end_value, step_value, body))
+    
+    def while_expr(self):
+        res = ParseResult()
+
+        res.register_advancement()
+        self.advance() 
+
+        condition = res.register(self.expr())
+        if res.error: return res
+
+        if not self.current_tok.matches(TT_KEYWORD, "då"):
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Förväntade \"då\""
+            ))
+        
+        res.register_advancement()
+        self.advance() 
+
+        body = res.register(self.expr())
+        if res.error: return res
+
+        return res.success(WhileNode(condition, body))
 
     def atom(self):
         res = ParseResult()
@@ -530,10 +622,15 @@ class Parser:
             if res.error: return res
             return res.success(if_expr)
         
-        elif tok.matches(TT_KEYWORD, "FÖR"):
+        elif tok.matches(TT_KEYWORD, "för"):
             for_expr = res.register(self.for_expr())
             if res.error: return res
             return res.success(for_expr)
+        
+        elif tok.matches(TT_KEYWORD, "medan"):
+            while_expr = res.register(self.while_expr())
+            if res.error: return res
+            return res.success(while_expr)
                     
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
@@ -595,7 +692,7 @@ class Parser:
                 return res.failure(InvalidSyntaxError(
                     self.current_tok.pos_start, self.current_tok.pos_end,
                     "Förväntade en identifierare"
-                ))
+                ))       
             
             var_name = self.current_tok
             res.register_advancement()
@@ -829,6 +926,14 @@ class Interpreter:
     def visit_VarAssignNode(self, node, context):
         res = RTResult()
         var_name = node.var_name_tok.value
+
+        if var_name in built_in_vars:
+            return res.failure(RTError(
+                node.pos_start, node.pos_end,
+                f"Kan inte ändra namn på den inbyggda variablen \"{var_name}\"",
+                context
+            ))
+
         value = res.register(self.visit(node.value_node, context))
         if res.error: return res
 
@@ -912,10 +1017,17 @@ class Interpreter:
             return res.success(else_value)
         
         return res.success(None)
+    
+    def visit_ForNode(self, node, context):
+        pass
 
 #######
 # RUN #
 #######
+
+built_in_vars = [
+    "tom", "sant", "falskt"
+]
 
 global_symbol_table = SymbolTable()
 global_symbol_table.set("tom", Number(0))
